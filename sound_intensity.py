5acbb6ba4c4a1fd861df43fc98840c28b3d5e971
junedmemon109 <85818251+junedmemon109@users.noreply.github.com>
2023-03-17 20:17:58 +0530

    Update sound_intensity.py

--- sound_intensity.py
+++ sound_intensity.py
@@ -7,5 +7,5 @@ def get_sound_intensity():
     # Read the sound intensity from the microphone
     sound = grovepi.analogRead(mic)
 
-    # Return the sound intensity as a value between 0 and 1023
+    # Return the sound intensity
     return sound
